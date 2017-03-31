da91ff09d0f72e9405738c40f7c88dc035ed7913
Abhishek Umesh Karanth <abhishek.umeshkaranth@amadeus.com>
2017-03-31 14:12:37 +0530

    Capture GET Info

--- app/controllers/ScalaController.scala
+++ app/controllers/ScalaController.scala
@@ -9,13 +9,15 @@ import play.api.db._
 
 class ScalaController @Inject()(db: Database) extends Controller {
 
-  def index = Action {
-    var outString = "Number from 9W is "
+  def index(carrier: Option[String]) = Action {
+    var outString = s"Pax Count for ${carrier.map(_.toString).getOrElse("")} is "
     val conn = db.getConnection()
-    
+
     try {
       val stmt = conn.createStatement
-      val rs = stmt.executeQuery("SELECT sum(pax_delta) as testkey from nh_ti_vistana_v2_4_p46_od1.booking_ond_activity_carrier_v24 where operating_carrier='9W'")
+      val queryStatement: String = s"SELECT sum(pax_delta) as testkey from nh_ti_vistana_v2_4_p46_od1.booking_ond_activity_carrier_v24 where operating_carrier='${carrier.map(_.toString).getOrElse("")}'"
+      println(queryStatement)
+      val rs = stmt.executeQuery(queryStatement)
 
       while (rs.next()) {
         outString += rs.getString("testkey")
